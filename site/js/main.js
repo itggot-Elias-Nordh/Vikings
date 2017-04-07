019fc605fb44a10441f58f102dd96336583e2472
itggot-Elias-Nordh <elias.nordh@itggot.se>
2017-04-07 12:54:33 +0200

    I finished the second page
    I put in all the episodes pictures and I made a knew function to go
    bettwen the difrent seasons episodes. I allso put down the copyright
    claim in the buttom of the page.

--- site/js/main.js
+++ site/js/main.js
@@ -2,3 +2,11 @@ $("a").click(function () {
     $("a").removeClass("blue");
     $(this).addClass("blue");
 });
+
+function season(number) {
+   document.querySelector(".season1").classList.add("bar");
+   document.querySelector(".season2").classList.add("bar");
+   document.querySelector(".season3").classList.add("bar");
+   document.querySelector(".season4").classList.add("bar");
+   document.querySelector(number).classList.remove("bar");
+}
